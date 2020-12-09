764778e601885c3b42ed6ff95a8f4617374a1106
zzh <hugyouzzh@gmail.com>
2020-12-09 17:30:49 +0800

    11

--- operation/operation/Test6.c
+++ operation/operation/Test6.c
@@ -18,31 +18,31 @@
 //		return  Fib(n - 1) + Fib(n - 2);
 //}
 
-int Fib(int n)
-{
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	while (n>2)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-}
-
-int main()
-{
-	int n = 0;
-	int ret = 0;
-	scanf("%d",&n);
-	ret = Fib(n);
-	printf("%d\n",ret);
-	//printf("%d\n", count);
-	return 0;
-}
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n>2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d",&n);
+//	ret = Fib(n);
+//	printf("%d\n",ret);
+//	//printf("%d\n", count);
+//	return 0;
+//}
 
 
 //int Fac1(int n)
