e25fbe678e21dd85fc39df1110a703fc014de8c4
zzh <hugyouzzh@gmail.com>
2020-12-14 22:19:26 +0800

    11

---
+++ Test1214/Test1214/Test.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+
+
+//int get_max(int x,int y)
+//{
+//	return x > y ? x : y;
+//}
+//
+//int main()
+//{
+//	int a = 50;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("%d",max);
+//	return 0;
+
+//struct Students
+//{
+//	char name[20];
+//	int age;
+//	int phonenumber;
+//};
+//
+//int main()
+//{
+//	struct Students s1 =
+//	{
+//		"ÕÅÈı",
+//		20,
+//		1111111
+//	};
+//	struct Students* ps = &s1;
+//	printf("%s",ps->name);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pc);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	*pa = 0;
+//	printf("%p\n",pa);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 10;
+//	char* pc = (char*)&n;
+//	int* pi = &n;
+//	printf("%p\n", &n);0
+//	printf("%p\n", pc);0
+//	printf("%p\n", pc+1);1
+//	printf("%p\n", pi);0
+//	printf("%p\n", pi+1);4
+//	return 0;
+//}
+
+
+int main()
+{
+	int* p;  
+	return 0;
+}
+
